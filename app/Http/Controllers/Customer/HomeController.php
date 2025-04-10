9100f091381bc5792fcb9b11428352ab0f646fb7
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-04-10 19:59:37 +0330

    sort products first part created

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -14,26 +14,49 @@ class HomeController extends Controller
     public function home()
     {
         // Auth::loginUsingId(1);
-        $slideShowImages = Banner::where('position',0)->where('status',1)->get();
-        $topBanners = Banner::where('position',1)->where('status',1)->take(2)->get();
-        $middleBanners = Banner::where('position',2)->where('status',1)->take(2)->get();
-        $bottomBanner = Banner::where('position',3)->where('status',1)->first();
+        $slideShowImages = Banner::where('position', 0)->where('status', 1)->get();
+        $topBanners = Banner::where('position', 1)->where('status', 1)->take(2)->get();
+        $middleBanners = Banner::where('position', 2)->where('status', 1)->take(2)->get();
+        $bottomBanner = Banner::where('position', 3)->where('status', 1)->first();
         $brands = Brand::all();
         $mostVisitedProducts = Product::latest()->take(10)->get();
         $offerProducts = Product::latest()->take(10)->get();
-        return view('customer.home',compact('slideShowImages','topBanners','middleBanners','bottomBanner','brands','mostVisitedProducts','offerProducts'));
+        return view('customer.home', compact('slideShowImages', 'topBanners', 'middleBanners', 'bottomBanner', 'brands', 'mostVisitedProducts', 'offerProducts'));
     }
 
     public function products(Request $request)
     {
-      
-        if($request->search)
-        {
-            
-            $products = Product::where('name', 'LIKE', "%" .$request->search. "%")->get();
 
+        switch ($request->sort) {
+            case "1":
+                $column = "created_at";
+                $direction = "DESC";
+                break;
+            case "2":
+                $column = "price";
+                $direction = "DESC";
+                break;
+            case "3":
+                $column = "price";
+                $direction = "ASC";
+                break;
+            case "4":
+                $column = "view";
+                $direction = "DESC";
+                break;
+            case "5":
+                $column = "sold_number";
+                $direction = "DESC";
+                break;
+            default:
+                $column = "created_at";
+                $direction = "ASC";
+                break;
+        }
+        if ($request->search) {
+
+            $products = Product::where('name', 'LIKE', "%" . $request->search . "%")->orderBy($column,$direction)->get();
         }
         return view('customer.market.product.products', compact('products'));
-
     }
 }
